2df6dc966ced579983aa502350abf44507752cbf
sidereusnuntius <sidereusnuntius@tuta.io>
2024-11-05 16:52:41 -0300

    Handling basic command line arguments.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,37 @@
+use std::{error::Error, io::Read};
+
+use ast::Statement;
+
 pub mod parser;
 mod ast;
+
+pub fn run<'a>(config: Config, source: &'a String) -> Result<(), Box<dyn Error + 'a>>{
+    let ast = parser::Parser::new().parse(&source)?;
+    ast.iter().map(|statement| println!("{:?}", *statement)).collect::<()>();
+    
+    Ok(())
+}
+
+pub struct Config {
+    pub file_name: String,
+    debug_mode: bool,
+    interpret: bool,
+}
+
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let file_name = if let Some(file) = args.next() {
+            file
+        } else {
+            return Err("No file name given.");
+        };
+
+        Ok(Config {
+            file_name,
+            debug_mode: false,
+            interpret: false,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use vit::parser;
+use std::{env, process, fs};
+
+use vit::{parser, run, Config};
 
 fn main() {
+    let args = env::args();
+    let config = match Config::build(args) {
+        Err(message) => {
+            eprintln!("{message}");
+            process::exit(1);
+        },
+        Ok(config) => config,
+    };
+
+    let source = match fs::read_to_string(&config.file_name) {
+        Ok(source) => source,
+        Err(message) => {
+            eprintln!("{message}");
+            process::exit(1);
+        }
+    };
     
-    let expr = parser::Parser::new()
-    .parse("c = 2 + -   2.0;")
-    .unwrap();
-    println!("{expr:?}");
+    if let Err(e) = run(config, &source) {
+        eprintln!("{:?}", e);
+    };
 }
